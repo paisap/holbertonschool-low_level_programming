bfe4766c43edc9c8251d807ef68caf4732fb5b6c
pasiap <santiagoaldana58@gmail.com>
2019-09-19 19:12:18 +0000

    first task advanced

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = 48; a < 58; a++)
+	{
+	for (b = a + 1; b < 58; b++)
+	{
+	putchar(a);
+	putchar(b);
+	if (a == 56 && b == 57)
+	{
+	}
+	else
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+}
+	putchar('\n');
+	return (0);
+}
