25499b748ae9e442797dedc0c9f108738498aadf
pasiap <santiagoaldana58@gmail.com>
2019-09-19 18:32:02 +0000

    answer task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 48; a < 58; a++)
+	{
+	putchar(a);
+	if (a == 57)
+	{
+	}
+	else
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	putchar('\n');
+	return (0);
+}
