e1f3a757b81fcf2142ca037be1312aa617730f5d
pasiap <santiagoaldana58@gmail.com>
2019-09-24 13:49:17 +0000

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,8 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
+ * print_alphabet - print the alphabet
  *
- * Return: Always 0.
  */
 void print_alphabet(void)
 {
