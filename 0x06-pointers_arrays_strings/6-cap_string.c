8e23265e92068b37a1d1bf464b0eda55255105d2
pasiap <santiagoaldana58@gmail.com>
2019-10-03 19:28:53 +0000

    task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+/**
+ * *cap_string - function that  concatenates two strings.
+ * @c: character validado
+ * Return: *char
+ */
+char *cap_string(char *c)
+{
+	int a, b;
+
+	b = 0;
+	if (c[0] >= 97 && c[0] <= 122)
+	{
+		c[0] = c[0] - 32;
+	}
+	a = 0;
+	while (c[a] != '\0')
+	{
+		if (c[a] == 32 || c[a] == '\t' || c[a] == '\n' || c[a] == ',')
+			b = 1;
+		if (c[a] == ';' || c[a] == '!' || c[a] == '?' || c[a] == 34)
+			b = 1;
+		if (c[a] == '(' || c[a] == ')' || c[a] == '{' || c[a] == '}')
+			b = 1;
+		if (b == 1)
+		{
+			if (c[a + 1] >= 97 && c[a + 1] <= 122)
+				c[a + 1] = c[a + 1] - 32;
+			b = 0;
+		}
+		a++;
+	}
+	return (c);
+}
--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -7,4 +7,5 @@ int _strcmp(char *s1, char *s2);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _putchar(char c);
+char *cap_string(char *);
 #endif
