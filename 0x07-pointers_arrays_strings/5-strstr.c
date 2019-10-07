6037dc70405fe4dead512d7fda79efb05e7cecf3
pasiap <santiagoaldana58@gmail.com>
2019-10-07 19:11:39 +0000

    task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -6,18 +6,27 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int x = 0, b = 0;
+	int x = 0, b = 0, c = 0, t = 0;
 
-	
 	while (haystack[x])
 	{
-		if(haystack[x] == needle[b])
+		if (haystack[x] == needle[b])
 		{
+			if (c == 0)
+				t = x;
 			x++;
 			c++;
 			b++;
 		}
 		else
+		{
 			x++;
+			b = 0;
+		}
 	}
-}
+	if (b == c)
+		haystack = (haystack + t);
+	else
+		haystack = 0;
+	return (haystack);
+	}
