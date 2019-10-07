46877ea74f23a022a2751c18ce425db5dc5eb070
pasiap <santiagoaldana58@gmail.com>
2019-10-07 21:39:40 +0000

    task 8

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,10 @@
+/**
+ * set_string - function that locates a substring.
+ * @s: The first string
+  * @to: The string concatenates
+ * Return: dest
+ */
+void set_string(char **s, char *to)
+{
+*s = to;
+}
