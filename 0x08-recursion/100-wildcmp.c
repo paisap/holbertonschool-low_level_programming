46883fdc7dd55d63137ef47825d3392dfd5a8e08
paisap <santiagoaldana58@gmail.com>
2019-10-10 03:08:59 +0000

    task advanced

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -1,16 +1,4 @@
 /**
- * wildcmp - compares two strings, supports wildcard '*'
- *
- * @s1: string 1 to compare to s2
- * @s2: string 2 to compare to s1
- *
- * Return: 1 if identical, 0 otherwise, -1 for error
- */
-int wildcmp(char *s1, char *s2)
-{
-	return (recursive_check(s1, s2, 0));
-}
-/**
  * recursive_check - checks for wild cards recursively
  *
  * @s1: string 1 to check (no wild cards)
@@ -44,4 +32,16 @@ int recursive_check(char *s1, char *s2, int wildC)
 	else if (*s1 != *s2 && wildC == 0)
 		return (0);
 	return (-1);
+}
+/**
+ * wildcmp - compares two strings, supports wildcard '*'
+ *
+ * @s1: string 1 to compare to s2
+ * @s2: string 2 to compare to s1
+ *
+ * Return: 1 if identical, 0 otherwise, -1 for error
+ */
+int wildcmp(char *s1, char *s2)
+{
+	return (recursive_check(s1, s2, 0));
 }
