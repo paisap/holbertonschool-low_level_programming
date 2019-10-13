a7e928bb72443266e3cb6cf9c9a52f389c566651
paisap <santiagoaldana58@gmail.com>
2019-10-13 04:57:17 +0000

    task advanzed.

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,21 +12,21 @@ int main(int argc, char *argv[])
 
 	if (argc != 2)
 	{
-		printf("Error");
+		printf("Error\n");
 		return (0);
 	}
 	x = atoi(argv[1]);
 	while (x > 0)
 	{
-		if (x >= 0)
+		if (x >= 25)
 			x = x - 25;
-		else if (x >= 0)
+		else if (x >= 10)
 			x = x - 10;
-		else if (x >= 0)
+		else if (x >= 5)
 			x = x - 5;
-		else if (x >= 0)
+		else if (x >= 2)
 			x = x - 2;
-		else if (x >= 0)
+		else if (x >= 1)
 			x = x - 1;
 		i++;
 	}
