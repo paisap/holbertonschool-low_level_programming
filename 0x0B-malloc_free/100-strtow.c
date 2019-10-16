22a1daece669c6606c248fdf12ab1da656048eef
paisap <santiagoaldana58@gmail.com>
2019-10-16 04:21:35 +0000

    task avanced

--- 0x0B-malloc_free/100-strtow.c
+++ 0x0B-malloc_free/100-strtow.c
@@ -1,74 +1,133 @@
+#include "holberton.h"
 #include <stdlib.h>
-int cp(char *str)
+/**
+ * _wordcount - count words in a string
+ * @s: string to count words
+ *
+ * Return: number of words in string
+ */
+int _wordcount(char *s)
 {
-	int i = 0, p = 0, b = 0;
+	int wordflag, count;
 
-	while(str[i])
-	{
-		if (str[i] != ' ')
+	wordflag = 0;
+	count = 0;
+	do {
+		if (wordflag == 0 && *s <= '~' && *s >= '!')
 		{
-			p++;
-			i++;
-			while (str[i] != ' ' && str[i] != '\0')
-				i++;
-			if (str[i] == '\0')
-			b = 1;
+			wordflag = 1;
+			count++;
 		}
-	if (b == 0)
-		i++;
-	}
-	return (p);
-}
-void free_grid(char **grid, int height)
-{
-	int i;
 
-	for (i = 0; i < height; i++)
-	{
-		free(grid[i]);
-	}
-	free(grid);
+		if (wordflag && (*s > '~' || *s < '!'))
+		{
+			wordflag = 0;
+		}
+	} while (*s++);
+	return (count);
 }
 /**
- * **strtow - function that  concatenates two strings.
- * @str: The first string
- * Return: dest
+ * _wordsize - return the size of word n in the string
+ * @s: string to look at
+ * @n: which word number to return
+ *
+ * Return: Length of word n.
+ */
+int _wordsize(char *s, int n)
+{
+	int wordflag, size;
+
+	wordflag = 0;
+	size = 0;
+	do {
+		if (wordflag == 0 && *s <= '~' && *s >= '!')
+		{
+			wordflag = 1;
+			n--;
+		}
+		if (wordflag && n == 0 && *s <= '~' && *s >= '!')
+		{
+			size++;
+		}
+
+		if (wordflag && (*s > '~' || *s < '!'))
+		{
+			wordflag = 0;
+		}
+	} while (*s++);
+	return (size);
+}
+/**
+ * _cpyarr - copy word into array based on string
+ * @s: string to take from
+ * @sdest: destination for word
+ * @n: word to copy
+ */
+void _cpyarr(char *s, char *sdest, int n)
+{
+	int wordflag, i, j;
+
+	wordflag = 0;
+	i = 0;
+	j = 0;
+	do {
+		if (wordflag == 0 && *(s + j) <= '~' && *(s + j) >= '!')
+		{
+			wordflag = 1;
+			n--;
+		}
+		if (wordflag && n == 0 && *(s + j) <= '~' && *(s + j) >= '!')
+		{
+			*(sdest + i) = *(s + j);
+			i++;
+		}
+
+		if (wordflag && (*(s + j) > '~' || *(s + j) < '!'))
+		{
+			wordflag = 0;
+		}
+		j++;
+	} while (*(s + j) != '\0');
+	*(sdest + i) = '\0';
+}
+
+/**
+ * strtow - Return a pointer to an array of strings
+ * @str: string to add
+ *
+ * Return: pointer to array of strings
  */
 char **strtow(char *str)
 {
-	char **rm;
-	int i = 0, j = 0, aux = 0, aux1 = 0, t = 0, ll = 0, s = 0;
+	int words, i;
+	char **arr;
 
-	aux = cp(str);
-	rm = malloc(sizeof(char *) * aux);
-	for (i = 0; i < aux; i++)
+	if (str == NULL || *str == '\0')
+		return (NULL);
+
+	words = _wordcount(str);
+
+	arr = malloc((words + 1) * sizeof(*arr));
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < words; i++)
 	{
-		aux1 = 0;
-		j = 0;
-		t = 0;
-		while (t == 0)
+		*(arr + i) = malloc((_wordsize(str, i + 1) + 1) * sizeof(char));
+		if (*(arr + i) == NULL)
 		{
-			if (str[j] != ' ')
-			{
-				s = j;
-				while (str[j] != ' ')
-				{
-					aux1++;
-					j++;
-					t = 1;
-				}
-			}
-			j++;
-		}
-		rm[i] = malloc(sizeof(char) * aux1);
-		if (rm[i] == NULL)
-		{
-			free_grid(rm, i);
+			for (i = i - 1; i >= 0; i--)
+				free(*(arr + i));
+			free(arr);
 			return (NULL);
-		}
-		for (ll = 0; ll < aux1; ll++)
-			rm[i][ll] = str[s];
-		}
-	return (rm);
-}
 
+		}
+	}
+	for (i = 0; i < words; i++)
+	{
+		_cpyarr(str, *(arr + i), i + 1);
+	}
+	*(arr + i) = NULL;
+
+	return (arr);
+}
