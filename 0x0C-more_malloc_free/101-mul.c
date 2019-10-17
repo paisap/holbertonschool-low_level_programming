0aa1db511b6f9fd8cb5a497418ea35127c405296
paisap <santiagoaldana58@gmail.com>
2019-10-17 16:54:42 -0500

    advanced for finish in home

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main (int argc, char *argv[])
+{
+	int x = 0, r = 0, tr = 0, may;
+	char *aux;
+
+	aux = mallo(20);
+	aux = argv[1];
+	while (aux[x] != '\0')
+		x++;
+	aux = argv[2];
+	while (aux[r] != '\0')
+		r++;
+	if (x >= y)
+		may = x;
+	else
+		may = r;
+	for (tr = 0; tr < may; tr++)
+
+	printf("argc %d argv [0]: %s\n", argc, argv[0]);
+	return(0);
+}
