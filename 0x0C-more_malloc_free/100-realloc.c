ad5c577e238c034dd61646732d929429819b8240
paisap <santiagoaldana58@gmail.com>
2019-10-17 14:12:11 -0500

    first advanced

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+/**
+ * *_realloc - function that  concatenates two strings.
+ * @ptr: The first string
+ * @old_size: the second string
+ * @new_size: new size
+ * Return: dest
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	int i = 0;
+	void *x;
+
+	if (ptr == NULL)
+		ptr = "";
+	if (old_size == new_size)
+		return (ptr);
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	x = malloc(new_size);
+	for (i = 0; ((char *)ptr)[i] != '\0'; i++)
+		((char *)x)[i] = ((char *)ptr)[i];
+	free(ptr);
+	return (x);
+}
