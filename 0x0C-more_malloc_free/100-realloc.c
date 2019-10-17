9958870f8403f8b6b7688c224e93e769ea473148
paisap <santiagoaldana58@gmail.com>
2019-10-17 15:13:50 -0500

    correction task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -12,7 +12,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	void *x;
 
 	if (ptr == NULL)
-		ptr = "";
+		return (malloc(new_size));
 	if (old_size == new_size)
 		return (ptr);
 	if (new_size == 0 && ptr != NULL)
