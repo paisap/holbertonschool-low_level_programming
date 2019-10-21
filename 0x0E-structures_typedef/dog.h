6be4323ec4314e1df6f5ba3f0d0a9a8dee2f147c
paisap <santiagoaldana58@gmail.com>
2019-10-21 12:25:24 -0500

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef dog_h
+#define dog_h
+/**
+ * struct dog - is a dog.
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+void init_dog(struct dog *d, char *name, float age, char *owner);
+#endif
