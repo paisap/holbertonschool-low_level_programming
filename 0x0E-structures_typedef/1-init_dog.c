86a6cdced1a4935ea67226fe139ec76ae014c23e
paisap <santiagoaldana58@gmail.com>
2019-10-21 13:19:28 -0500

    correction task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,8 +1,17 @@
 #include "dog.h"
+#include <stdlib.h>
 
+/**
+ * init_dog - function that gets the length of a prefix substring.
+ * @d: The first string
+ * @name: The string concatenates
+ * @age: edad
+ * @owner: duenio
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	
+	if (d == NULL)
+		exit(0);
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
