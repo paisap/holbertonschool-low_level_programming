4b7ca7e6a80dfaf57fa998a76536a2ea80a0848a
paisap <santiagoaldana58@gmail.com>
2019-10-21 14:06:25 -0500

    task2.2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,7 +16,7 @@ void print_dog(struct dog *d)
 			printf("Name: (nil)\n");
 		else
 			printf("Name: %s\n", d->name);
-		if (!d->age)
+		if (d->age == 0.0)
 			printf("Age: (nil)");
 		else
 			printf("Age: %f\n", d->age);
