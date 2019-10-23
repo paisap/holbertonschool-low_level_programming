1f6463692b34317e911831aad8c5dbc51c4f987f
paisap <santiagoaldana58@gmail.com>
2019-10-23 16:29:53 -0500

    task 3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,17 +1,28 @@
+#include "3-calc.h"
+#include <stddef.h>
+/**
+ * get_op_func - entry
+ * @s: char
+ * Return: algo
+ */
 int (*get_op_func(char *s))(int, int)
 {
 op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i;	
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
+	};
+	int i;
 
-    for ( i = 0; i < 6; i++)
-    {
-	    if (*s == ops[i]->op)
-	    {
-
+	for (i = 0; i < 6; i++)
+	{
+		if (*s == ops[i].op[0])
+		{
+			return  (ops[i].f);
+		}
+	}
+	return (NULL);
+}
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,6 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
 /**
  * main - check the code for Holberton School students.
- *
+ * @argc: argc.
+ * @argv: argv
  * Return: Always 0.
  */
 int main(int argc, char *argv[])
@@ -13,15 +17,16 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 	x = *argv[2];
-	if (x != 43 || x != 45 || x != 42 || x != 47 || x != 37)
+	if (x != 43 && x != 45 && x != '*' && x != 47 && x != 37)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if ((x == 47 && argv[3] == 0) || (x == 37 &&  argv[3] == 0))
+	if ((x == 47 && atoi(argv[3]) == 0) || (x == 37 && atoi(argv[3]) == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
+	printf("%d\n", (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3])));
+	return (0);
 }
-
