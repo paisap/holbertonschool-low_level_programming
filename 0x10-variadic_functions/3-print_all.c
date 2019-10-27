6b4a6716a9d0def1c0402f65cccef80f4b2418aa
paisap <santiagoaldana58@gmail.com>
2019-10-27 21:03:58 +0000

    finish..

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include "variadic_functions.h"
+
 /**
  * p_str - print string
  * @list: list
@@ -8,12 +9,15 @@
 void p_str(va_list list)
 {
 	char *b;
-	b = va_arg(list, char *);
 
+	b = va_arg(list, char *);
 	if (b == NULL)
+	{
 		b = "(nil)";
+	}
 	printf("%s", b);
 }
+
 /**
  * p_float - print float
  * @list: list
@@ -22,6 +26,7 @@ void p_float(va_list list)
 {
 	printf("%f", va_arg(list, double));
 }
+
 /**
  * p_char - print char
  * @list: list
@@ -30,6 +35,7 @@ void p_char(va_list list)
 {
 	printf("%c", va_arg(list, int));
 }
+
 /**
  * p_int - print int
  * @list: list
@@ -38,6 +44,7 @@ void p_int(va_list list)
 {
 	printf("%d", va_arg(list, int));
 }
+
 /**
  * print_all - suma
  * @format: The first string
@@ -46,11 +53,11 @@ void p_int(va_list list)
 void print_all(const char * const format, ...)
 {
 	forma_t ops[] = {
-		{'c', p_char},
-		{'i', p_int},
-		{'f', p_float},
-		{'s', p_str},
-	};
+			{'c', p_char},
+			{'i', p_int},
+			{'f', p_float},
+			{'s', p_str},
+		};
 	int i = 0, j;
 	char *sc = "";
 	va_list list;
