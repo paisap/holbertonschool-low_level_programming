5464967a709f4a532c528e1c0c9f3dbaf2121f95
paisap <santiagoaldana58@gmail.com>
2019-11-05 10:38:25 -0500

    try task 9.3

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,7 +23,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	aux->n = n;
 	if (idx == 0)
 	{
-		aux->next = (*head)->next;
+		aux->next = (*head);
 		(*head) = aux;
 		return (aux);
 	}
