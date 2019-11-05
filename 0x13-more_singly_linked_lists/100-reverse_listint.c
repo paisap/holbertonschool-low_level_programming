6c85a94bb1b744203122d7420ef6202ae1be6597
paisap <santiagoaldana58@gmail.com>
2019-11-05 14:40:39 -0500

    task 11

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,30 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * *reverse_listint - writes the character c to stdout
+ * @head: The character to print
+ *
+ * Return: list
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *aux = *head;
+	listint_t *aux1;
+
+	(*head) = (*head)->next;
+	aux->next = NULL;
+	aux1 = *head;
+	while ((*head)->next != NULL)
+	{
+
+		(*head) = (*head)->next;
+		aux1->next = aux;
+		aux = aux1;
+		aux1 = *head;
+	}
+	(*head)->next = aux;
+	return (*head);
+}
