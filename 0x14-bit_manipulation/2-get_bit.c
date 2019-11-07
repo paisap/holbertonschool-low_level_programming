613d0f22c3e66252fefba62afb5431d712852c3a
paisap <santiagoaldana58@gmail.com>
2019-11-07 13:26:49 -0500

    task 0 to 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,42 +1,23 @@
 #include "holberton.h"
 /**
- * get_bin - function that draws a straight line in the terminal.
+ * get_bit - function that draws a straight line in the terminal.
  * @n: The number to be checked
  * @index: the posicion
  * Return: the byte
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int aux = 4096, b = 0;
-	unsigned long int t = 0, m = 0;
-	int x = 0;
-	int s[30];
+	unsigned long int x = 0;
 
-	if (n == t)
-		s[m] = 0, m++;
-	else
+	if (n == 0 && index == 0)
+		return (0);
+
+	while (x < index)
 	{
-		while (aux != 0)
-		{
-			x = n - aux;
-			if (x >= 0)
-			{
-				b = 1;
-				s[m] = 1;
-				m++;
-				n = n - aux;
-			}
-			else
-			{
-				if (b == 1)
-					s[m] = 0, m++;
-			}
-			aux = aux * 0.5;
-		}
+		n = n >> 1;
+		if (n == 0)
+			return (-1);
+		x++;
 	}
-	if (index >= m)
-		return (-1);
-	x = s[m - (index + 1)];
-	return (x);
-
+	return (n & 1);
 }
