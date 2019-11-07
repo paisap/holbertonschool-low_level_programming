625939ee725c675561d99c2554a69d706ff8a6f0
paisap <santiagoaldana58@gmail.com>
2019-11-07 14:58:43 -0500

    task 4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,13 @@
+/**
+ * clear_bit - function that draws a straight line in the terminal.
+ * @n: The number to be checked
+ * @index: number
+ * Return: number
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 64)
+		return (-1);
+	*n = *n & ~(1 << index);
+return (1);
+}
