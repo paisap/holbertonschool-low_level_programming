f762a52d5d0f3072a537521305ff1dcb2a74481a
paisap <santiagoaldana58@gmail.com>
2019-11-07 13:36:42 -0500

    task 2 try correction.

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,7 +7,8 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if ( index > sizeof(unsigned int) * 8)
+
+	if (index > sizeof(unsigned int) * 8)
 			return (-1);
 	else
 		return ((n >> index) & 1);
