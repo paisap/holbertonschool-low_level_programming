412309925eed7eed7c303b46c4284cc0c0348297
paisap <santiagoaldana58@gmail.com>
2019-11-12 12:50:58 -0500

    task 1.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,11 +13,18 @@ int create_file(const char *filename, char *text_content)
 
 	if (t == -1)
 		return (-1);
+
 	if (filename == NULL)
 		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+
 	while (text_content[i] != '\0')
 		i++;
+
 	if (write(t, text_content, i) == -1)
 		return (-1);
+
 	return (1);
 }
