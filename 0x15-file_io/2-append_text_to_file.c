230778d6efc3a832f61997ef22e1efe996af9920
paisap <santiagoaldana58@gmail.com>
2019-11-12 13:09:46 -0500

    task 2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * create_file - function that draws a straight line in the terminal.
+ * @filename: The number to be checked
+ * @text_content: a mounth
+ *
+ * Return: aux
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int t = open(filename, O_WRONLY | O_APPEND, 0600), i = 0;
+
+	if (t == -1)
+		return (-1);
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+
+	while (text_content[i] != '\0')
+		i++;
+
+	if (write(t, text_content, i) == -1)
+		return (-1);
+
+	return (1);
+}
