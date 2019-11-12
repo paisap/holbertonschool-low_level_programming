59e7cc95f76403cd96b8906b715a8947b45b0fad
paisap <santiagoaldana58@gmail.com>
2019-11-12 12:04:17 -0500

    soy un idiota part 2

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,19 +14,23 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL)
 		return (0);
+
 	t = open(filename, O_RDONLY);
 	if (t == -1)
 		return (0);
+
 	buf = malloc(letters);
 	if (buf == NULL)
 		return (0);
 
-	aux = read(t, buf, (letters));
+	aux = read(t, buf, letters);
 	if (aux == -1)
 		return (0);
-	aux = write(1, buf, aux);
+
+	aux = write(STDOUT_FILENO, buf, aux);
 	if (aux == -1)
 		return (0);
+
 	close(t);
 	return (aux);
 }
