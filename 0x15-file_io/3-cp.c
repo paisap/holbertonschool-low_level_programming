90c0fb107b418c93a4fbbbe37607c9914f523004
paisap <santiagoaldana58@gmail.com>
2019-11-12 17:39:40 -0500

    try task 3o

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -19,9 +19,12 @@ int main(int ac, char **av)
 	t = open(av[1], O_RDONLY);
 	if (t == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
-	p = open(av[2], O_CREAT | O_RDWR | O_TRUNC | O_APPEND, 0664);
+	p = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (p == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		close(t), exit(99);
+	}
 	while (aux == 1024)
 	{
 		aux = read(t, buf, 1024);
@@ -32,22 +35,19 @@ int main(int ac, char **av)
 		}
 		aux1 = write(p, buf, aux);
 		if (aux1 < aux)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-			exit(99);
-		}
-
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 	}
 	if (close(p) == -1)
 	{
+		close(t);
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", p);
 		exit(100);
 	}
 	if (close(t) == -1)
 	{
+		close(p);
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", t);
 		exit(100);
 	}
 	return (1);
-
 }
