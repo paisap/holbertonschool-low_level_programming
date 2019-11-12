38c560bc2ac914eb51f4f2701fde8e39aea37476
paisap <santiagoaldana58@gmail.com>
2019-11-12 12:47:53 -0500

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,23 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * create_file - function that draws a straight line in the terminal.
+ * @filename: The number to be checked
+ * @text_content: a mounth
+ *
+ * Return: aux
+ */
 int create_file(const char *filename, char *text_content)
 {
-	int t = open(filename, O_CREAT);
+	int t = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600), i = 0;
 
+	if (t == -1)
+		return (-1);
+	if (filename == NULL)
+		return (-1);
+	while (text_content[i] != '\0')
+		i++;
+	if (write(t, text_content, i) == -1)
+		return (-1);
+	return (1);
+}
