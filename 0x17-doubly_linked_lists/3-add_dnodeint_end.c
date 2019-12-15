e17f342fb54a06ab6fc38c3906f2ad944e35fb38
paisap <santiagoaldana58@gmail.com>
2019-12-15 20:38:50 +0000

    task3

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+/**
+ * *add_dnodeint_end - writes the character c to stdout
+ * @head: The character to print
+ * @n: name
+ *
+ * Return: list
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *aux, *new;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	aux = *head;
+	if (*head == NULL)
+	{
+		*head = new;
+		new->next = NULL;
+		new->prev = *head;
+	}
+	else
+	{
+		while (aux->next != NULL)
+			aux = aux->next;
+		aux->next = new;
+		new->next = NULL;
+		new->prev = aux;
+	}
+	return (new);
+}
