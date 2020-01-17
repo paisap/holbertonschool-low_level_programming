afaf8b07bfbc0c31a6c2e1c2ebb279e3e21969c0
paisap <santiagoaldana58@gmail.com>
2020-01-17 22:38:19 +0000

    project

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new = NULL;
+
+	new = malloc(sizeof(hash_table_t));
+	if (new == NULL)
+		return (NULL);
+	new->size = size;
+	new->array = malloc(size * 8);
+	if (new->array == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	return (new);
+}
