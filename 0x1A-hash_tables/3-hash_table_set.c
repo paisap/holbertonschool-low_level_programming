efea728660761f205f4d83d3142c1e37765b5099
paisap <santiagoaldana58@gmail.com>
2020-01-19 02:38:41 +0000

    task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,37 +9,24 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int x = 0;
-	int y = 0;
 	hash_node_t *new_nodo;
 
 	if (key == NULL || key[0] == '\0')
 		return (0);
 	new_nodo = malloc(sizeof(hash_node_t));
 	if (new_nodo == NULL)
-		return (0);
-	while (value[y])
-		y++;
-	new_nodo->value = malloc(y);
-	if (new_nodo->value == NULL)
-	{
-		free(new_nodo);
-		return (0);
-	}
-	y = 0;
-	while (key[y])
-		y++;
-	new_nodo->key = malloc(y);
-	if (new_nodo->key == NULL)
-	{
-		free(new_nodo->value);
-		free(new_nodo);
-		return (0);
-	}
-	strcpy(new_nodo->key, key);
-	strcpy(new_nodo->value, value);
+		return (0);	
 	x = key_index((const unsigned char *)key, ht->size);
 	if (x > ht->size)
 		return (0);
+	if (strcmp(ht->array[x]->key, key))
+	{
+		free(ht->array[x]->value);
+		ht->array[x]->value = strdup(value);
+	}
+	new_nodo->value = strdup(value);
+	new_nodo->key = strdup(key);
+	new_nodo->next = ht;
 	ht->array[x] = new_nodo;
 	return (1);
 }
