63ad462fdb42baf005f06968530f4f86d38bcc0c
paisap <santiagoaldana58@gmail.com>
2020-02-21 20:38:13 +0000

    task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,12 +4,24 @@ def island_perimeter(grid):
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
-                if grid[i][j + 1] == 0:
+                try:
+                    if grid[i][j + 1] == 0:
+                        cont += 1
+                except IndexError:
                     cont += 1
-                if grid[i][j - 1] == 0:
+                try:
+                    if grid[i][j - 1] == 0:
+                        cont += 1
+                except IndexError:
                     cont += 1
-                if grid[i + 1][j] == 0:
+                try:
+                    if grid[i + 1][j] == 0:
+                        cont += 1
+                except IndexError:
                     cont += 1
-                if grid[i -1][j] == 0:
+                try:
+                    if grid[i -1][j] == 0:
+                        cont += 1
+                except IndexError:
                     cont += 1
     return cont
