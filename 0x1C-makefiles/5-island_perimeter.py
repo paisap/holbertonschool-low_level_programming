d8a77fc6bd9b36106c1f02813d14a01fe24016a3
paisap <santiagoaldana58@gmail.com>
2020-02-23 22:41:03 +0000

    finish

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,33 +1,29 @@
 #!/usr/bin/python3
+"""
+    island
+"""
+
+
 def island_perimeter(grid):
+    """ .... """
     cont = 0
-    aux = 0
     for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            if grid[i][j] == 0:
-                if grid[i][j + 1] == 1:
-                    aux += 1
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
+        for j in range(len(grid[0])):
             if grid[i][j] == 1:
-                try:
-                    if grid[i][j + 1] == 0:
-                        cont += 1
-                except IndexError:
-                    cont += 1
-                try:
-                    if grid[i][j - 1] == 0:
-                        cont += 1
-                except IndexError:
-                    cont += 1
-                try:
-                    if grid[i + 1][j] == 0:
-                        cont += 1
-                except IndexError:
-                    cont += 1
-                try:
-                    if grid[i -1][j] == 0:
-                        cont += 1
-                except IndexError:
-                    cont += 1
+                cont += chek_cont(i, j, grid)
+    return cont
+
+
+def chek_cont(i, j, grid):
+    cont = 0
+    height = len(grid) - 1
+    width = len(grid[0]) - 1
+    if i - 1 < 0 or grid[i - 1][j] == 0:
+        cont += 1
+    if i + 1 > height or grid[i + 1][j] == 0:
+        cont += 1
+    if j - 1 < 0 or grid[i][j - 1] == 0:
+        cont += 1
+    if j + 1 < width or grid[i][j + 1] == 0:
+        cont += 1
     return cont
